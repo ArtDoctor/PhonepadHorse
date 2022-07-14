81105426cc68e409f18e0f0d716c730beac4add5
Ash <46786959+ArtDoctor@users.noreply.github.com>
2022-07-14 15:41:26 +0200

    Final final commit

--- main.py
+++ main.py
@@ -1,14 +1,12 @@
 class problemSolver:
     # Possible moves for different positions on numpad
-    a = [[4, 6],
-         [6, 8], [7, 9], [4, 8],
-         [3, 9, 0], [], [1, 7, 0],
-         [2, 6], [1, 3], [2, 4]]
-
-    def __init__(self):
-        pass
+    numPadMoves = [[4, 6],
+                   [6, 8], [7, 9], [4, 8],
+                   [3, 9, 0], [], [1, 7, 0],
+                   [2, 6], [1, 3], [2, 4]]
 
     def solve(self, position, n):
+        # In position 5 there are no possible moves
         if position == 5:
             return 1
         else:
@@ -16,18 +14,21 @@ class problemSolver:
             hop = 0
             print("Start position:", possiblePositions)
             while hop < n:
+                # Every hop we calculate for each possible position possible positions
                 hop += 1
                 tempArray = []
                 for i in possiblePositions:
-                    tempArray += self.a[i]
+                    tempArray += self.numPadMoves[i]
                 possiblePositions = tempArray
                 print("Hop", hop, ":", possiblePositions)
+            # Result is the amount of possible positions
             res = len(possiblePositions)
+            # They will be distinct for sure
             return res
 
 
 solver = problemSolver()
-print(solver.solve(3, 3))
+print(solver.solve(1, 3))
 
 # Tests:
 """
